0facaf33b8fa4a480c63cc32668cccaa68f0cc66
Mba Thomas <angelfab631@gmail.com>
2026-03-30 13:50:14 +0100

    Create success.php

---
+++ app/views/pages/success.php
@@ -0,0 +1,11 @@
+<?php require APPROOT . '/views/inc/head.php'?>
+<?php require APPROOT . '/views/inc/navbar.php'?>
+
+<div class="container mt-3">
+<div class="jumbotron jumbotron-fluid text-center">
+      <h1 class="text-uppercase"><?= $data['title'] ?></h1>
+      <p class="lead"><?= $data['description'] ?></p>
+</div>
+</div>
+
+<?php require APPROOT . '/views/inc/footer.php'?>
